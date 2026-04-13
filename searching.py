4e30241d58f6d4df70056461158c44f6832ee504
matojaotam <lalokonosec1@gmail.com>
2026-04-13 14:18:51 +0200

    generatory

--- searching.py
+++ searching.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import json
-
+import time
+from generators import unordered_sequence, ordered_sequence
 
 def read_data(file_name, field):
 
@@ -44,6 +45,32 @@ def binary_search(num_list, num_search):
         else:
             prava = mid - 1
     return None
+#
+import matplotlib.pyplot as plt         #zkusebni graf
+#
+# sizes = [100, 500, 1000, 5000, 10000]
+# times = [0.00001, 0.00003, 0.00006, 0.00031, 0.00067]
+#
+# plt.plot(sizes, times)
+#
+# plt.xlabel("Velikost vstupu")
+# plt.ylabel("Čas [s]")
+# plt.title("Ukázkový graf měření")
+# plt.show()
+
+def compare_search():
+    velikosti = [100, 500, 1000, 5000, 10000]
+
+    linear_times = []
+    binary_times = []
+
+    for velikost in velikosti:
+
+        unordered = unordered_sequence(velikost)
+        ordered = ordered_sequence(velikost)
+
+        target = unordered[-1]
+
 
 
 
