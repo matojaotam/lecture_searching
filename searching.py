662175f1118ce02b6a79c74fd2b42c8b6d5c7399
matojaotam <lalokonosec1@gmail.com>
2026-04-13 13:16:53 +0200

    nacteni a prvni podminka

--- searching.py
+++ searching.py
@@ -3,20 +3,14 @@ import json
 
 
 def read_data(file_name, field):
-    """
-    Reads a JSON file and returns data for a given field.
 
-    Args:
-        file_name (str): Name of the JSON file.
-        field (str): Key to retrieve from the JSON data.
-            Must be one of: 'unordered_numbers', 'ordered_numbers' or 'dna_sequence'.
+    with open(file_name, "r") as file:
+        data = json.load(file)
+    if field not in data:
+        return None
 
-    Returns:
-        list | str | None:
-            - list: If data retrieved by the selected field contains numeric data.
-            - str: If field is 'dna_sequence'.
-            - None: If the field is not supported.
-    """
+    return data[field]
+
     # get current working directory path
     cwd_path = Path.cwd()
     
@@ -24,8 +18,6 @@ def read_data(file_name, field):
 
 
 def main():
-    pass
 
 
-if __name__ == "__main__":
-    main()
+
